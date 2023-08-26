69c38856b3016245806aa3594af2b592b519648b
venfriti <tolusanusi6@gmail.com>
2023-08-26 12:01:49 +0100

    add unit tests

--- app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -5,10 +5,15 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.udacity.project4.locationreminders.data.FakeDataSource
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.CoreMatchers.nullValue
+import com.udacity.project4.locationreminders.getOrAwaitValue
+import org.hamcrest.core.IsNot.not
+import org.junit.Assert.assertThat
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
@@ -24,6 +29,7 @@ class RemindersListViewModelTest {
 
     @Before
     fun setUpViewModel() {
+        stopKoin()
 
         reminderRepository = FakeDataSource()
 
@@ -37,6 +43,14 @@ class RemindersListViewModelTest {
     @Test
     fun loadRemindersWhenUnavailable_causesError(){
         //Given a fresh ViewModel
+
+        reminderRepository.setReturnError(true)
+
+        // WHEN - we want to load rhe reminders
+        remindersListViewModel.loadReminders()
+
+        // THEN - It's an error, there's a snackbar
+        assertThat(remindersListViewModel.showSnackBar.getOrAwaitValue(), not(nullValue()))
 
 
     }
